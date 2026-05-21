af23524b5d342c004c56c15caf002cd5ddd057ff
Ibe Enoch Okechukwu <ibeenochokey@gmail.com>
2026-05-21 15:39:10 +0100

    update

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
+// Show food menu
 
+const store = ["Indomie", "Rice", "Garri", "Beans", "Banana","Turkey", "Moi-Moi"]
+
+document.getElementById("menu-btnm").onclick = () =>{
+    const menu = document.getElementById("menu")
+    const menuhead = document.getElementById("menuhead")
+    const note = document.getElementById("re")
+    menu.classList.toggle("menu")
+    menu.classList.add("menu-show")
+    menuhead.textContent = 'food-list.'
+    note.textContent = 'eat like a king.'
+}
+  for(let i = 0; i < store.length; i++){
+        const li = document.createElement("li")
+        li.textContent = i  + '. ' + store[i]
+        menu.appendChild(li)
+    }
+
+// Show food cheaker
+document.getElementById("menu-btnc").onclick = () =>{
+    const foodName = document.getElementById("foodcheck")
+    const title = document.getElementById("check")
+   foodName.classList.toggle("foodcheck")
+   foodName.classList.add("foodcheck-show")
+   title.textContent = "check food by name."
+
+}
+
+// Check food availability
 const btn = document.getElementById("btn")
 const foodInput = document.getElementById("input")
 const cart = document.getElementById("cart")
 const p = document.createElement("p")
-
-const store = ["Indomie", "Rice", "Garri", "Beans", "Banana","Turkey", "Moi-Moi"]
 
 let isAvailable  = false
 
@@ -18,7 +45,7 @@ const order = foodInput.value
         cart.classList.add("error")
         foodInput.classList.add("error")
         return
-        //the RETURN keyword prevents the loop from running if the ORDER field is empty
+        // the RETURN keyword prevents the loop from running if the ORDER field is empty
     }
     
     for(let i = 0; i < store.length; i++){
@@ -29,13 +56,13 @@ const order = foodInput.value
 }
 
     if(isAvailable){
-        p.textContent = order + ' Is Available'
+        p.textContent = `${order}  Is Available`
         cart.appendChild(p)
         cart.classList.add("accept")
         cart.classList.remove("error")
         foodInput.classList.remove("error")
     }else{
-        p.textContent = order + ' Is Not Available'
+        p.textContent = `${order}  Is Not Available`
         cart.appendChild(p)
         cart.classList.add("accept")
         cart.classList.remove("error")
@@ -44,16 +71,17 @@ const order = foodInput.value
 
 })
 
-
-
    foodInput.onclick =()=>{
      foodInput.value = ''
      p.textContent = ''
     cart.classList.remove("accept")
+    cart.classList.remove("error")
 }
 
 
 
+
+                                            // DEMO CODE
 
 // if(isAvailable ){
 //     console.log("Available")
