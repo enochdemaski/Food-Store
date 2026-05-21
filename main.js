cf34857451ed51a3d99f4b8b05535d239adb3af8
Ibe Enoch Okechukwu <ibeenochokey@gmail.com>
2026-05-21 08:51:20 +0100

    working app, but stiil needs update

--- main.js
+++ main.js
@@ -1,81 +1,55 @@
 
-// const btn = document.getElementById("btn")
-// const order = document.getElementById("input")
-// const cart = document.getElementById("cart")
-// const p = document.createElement("p")
-
-const prompt = require("prompt-sync")()
+const btn = document.getElementById("btn")
+const foodInput = document.getElementById("input")
+const cart = document.getElementById("cart")
+const p = document.createElement("p")
 
 const store = ["Indomie", "Rice", "Garri", "Beans", "Banana","Turkey", "Moi-Moi"]
-
-const order = prompt("enter: ")
 
 let isAvailable  = false
 
 
-for(let i = 0; i < store.length; i++){
-    if(store[i] == order){
-        isAvailable == true
+btn.addEventListener("click", ()=>{
+const order = foodInput.value
+
+ if(order == ''){
+        p.textContent ='Please Enter Your Chioce Of Food.'
+        cart.appendChild(p)
+        cart.classList.add("error")
+        foodInput.classList.add("error")
+        return
+        //the RETURN keyword prevents the loop from running if the ORDER field is empty
+    }
+    
+    for(let i = 0; i < store.length; i++){
+    if(store[i].toLowerCase() == order.toLowerCase()){
+        isAvailable = true
         break
     }
 }
 
-if(isAvailable ){
-    console.log("Available")
-}else{
-    console.log(" Not available")
-}
+    if(isAvailable){
+        p.textContent = order + ' Is Available'
+        cart.appendChild(p)
+        cart.classList.add("accept")
+        cart.classList.remove("error")
+        foodInput.classList.remove("error")
+    }else{
+        p.textContent = order + ' Is Not Available'
+        cart.appendChild(p)
+        cart.classList.add("accept")
+        cart.classList.remove("error")
+        foodInput.classList.remove("error")
+    }
+})
 
 
 
-
-
-// btn.addEventListener("click", () =>{
-
-// if(isAvailable){
-//     cart.textContent = "Please Enter Your Food"
-//     // cart.appendChild(p)
+// if(isAvailable ){
+//     console.log("Available")
+// }else{
+//     console.log(" Not available")
 // }
-
-
-
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
